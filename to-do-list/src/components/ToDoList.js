334327df155cea786f961baf05cc9664e255641e
paulorick7 <32898288+paulorick7@users.noreply.github.com>
2023-07-17 14:27:26 +0100

    start creating to-do-list

---
+++ to-do-list/src/components/ToDoList.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import "../css/ToDoList.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function ToDoList() {
+    const [tasks, setTasks] = useState([]);
+    const [valueTask, setValueTask] = useState("");
+    const [counter, setCounter] = useState(1);
+
+    const handleValueTaskChange = (event) => {
+        setValueTask(event.target.value);
+    };
+
+    const handleAddTask = () => {
+        if (valueTask.trim() !== "") {
+
+            setTasks([...tasks, counter + ". " + valueTask]);
+            setValueTask("");
+            setCounter(counter + 1);
+        }
+    };
+    const handleRemoveTask = (index) => {
+        const newTasksArray = [...tasks];
+        newTasksArray.splice(index, 1);
+        setTasks(newTasksArray);
+        setCounter(counter - 1);
+    };
+
+    return (
+        <div>
+            <h1 id="title">To Do List</h1>
+            <div className="input-group mb-3 task-div" >
+                <input type="text" className="form-control" value={valueTask} placeholder="Add a task" onChange={handleValueTaskChange} aria-label="Recipient's username" ></input>
+                    <div className="input-group-append">
+                        <button onClick={handleAddTask} className="btn btn-success" type="button">Add Task</button>
+                    </div>
+            </div>
+
+            {/* <div className='task-div'>
+                <input type='text' value={valueTask} placeholder='Add a task' onChange={handleValueTaskChange}></input>
+                <button onClick={handleAddTask} className='btn btn-success'>Add task</button>
+            </div> */}
+
+            {tasks.length !== 0 ?
+                <ol className='task-list'>
+                    {tasks.map((task, index) => (
+                        <li key={index}>
+                            <label>{task}</label>
+                            <button className="remove-button btn btn-danger" onClick={() => handleRemoveTask(index)}>Remove</button>
+                        </li>
+                    ))}
+                </ol> : <ul className='task-list'>
+                    <h4>No items to show</h4>
+                </ul>}
+        </div>
+    );
+}
