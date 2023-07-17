db21a800c91fcf61700d087977c8e0ad2cbe465b
paulorick7 <32898288+paulorick7@users.noreply.github.com>
2023-07-17 21:39:51 +0100

    fixed bugs container

--- to-do-list/src/components/ToDoList.js
+++ to-do-list/src/components/ToDoList.js
@@ -28,28 +28,31 @@ export default function ToDoList() {
 
     return (
         <div>
+            <div className='input-group mb-3 title-div'>
             <h1 id="title">To Do List</h1>
+            
             <div className="input-group mb-3 task-div" >
-                <input type="text" className="form-control" value={valueTask} placeholder="Add a task" onChange={handleValueTaskChange} aria-label="Recipient's username" ></input>
+                <input type="text" className="form-control" value={valueTask} placeholder="Enter a task" onChange={handleValueTaskChange} aria-label="Recipient's username" ></input>
                     <div className="input-group-append">
                         <button onClick={handleAddTask} className="btn btn-success" type="button">Add Task</button>
                     </div>
-            </div>
-
+            </div></div>
             {/* <div className='task-div'>
                 <input type='text' value={valueTask} placeholder='Add a task' onChange={handleValueTaskChange}></input>
                 <button onClick={handleAddTask} className='btn btn-success'>Add task</button>
             </div> */}
 
             {tasks.length !== 0 ?
-                <ol className='task-list'>
+                <ul className='task-list'>
                     {tasks.map((task, index) => (
+                        <div className='li-div'>
                         <li key={index}>
                             <label>{task}</label>
                             <button className="remove-button btn btn-danger" onClick={() => handleRemoveTask(index)}>Remove</button>
                         </li>
+                        </div>
                     ))}
-                </ol> : <ul className='task-list'>
+                </ul> : <ul className=' no-items'>
                     <h4>No items to show</h4>
                 </ul>}
         </div>
